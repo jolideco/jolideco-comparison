4de381749d0c6eeb07991016e993194f0e4db7b2
Axel Donath <axel.donath@cfa.harvard.edu>
2023-04-05 11:08:41 -0400

    Specify cluster ressources

---
+++ workflow/cluster/qsub-submit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from snakemake.utils import read_job_properties
+
+jobscript = sys.argv[1]
+job_properties = read_job_properties(jobscript)
+
+name_template = "{scenario}-{bkg_level}-{prefix}-{method}"
+name = job_properties["rule"] + name_template.format(**job_properties["wildcards"])
+
+ARGS = {
+    "-S": "/bin/bash",
+    "-pe": "mthread 16",
+    "-q": "mThC.q",
+    "-l": "mres=32G,h_data=4G,h_vmem=4G",
+    "-j": "y",
+    "-N": f"{name}",
+    "-M": "axel.donath@cfa.harvard.edu",
+}
+
+args = " ".join(f"{key} {value}" for key, value in ARGS.items())
+
+os.system(f"qsub {args} {jobscript}")

8dd70325ec7eb000472a0e283b71bb35898ab860
Axel Donath <axel.donath@cfa.harvard.edu>
2023-04-07 11:46:50 -0400

    Add debug to config

--- workflow/scripts/run-comparison.py
+++ workflow/scripts/run-comparison.py
@@ -3,9 +3,9 @@ import copy
 import numpy as np
 from utils import read_config, read_datasets, stack_datasets
 
-DEBUG = True
-
 RANDOM_STATE = np.random.RandomState(7362)
+
+N_ITER_DEBUG = 5
 
 
 def prepare_datasets_lira(datasets):
@@ -38,7 +38,7 @@ def get_flux_init(datasets, oversample=10.0):
     return flux_init.astype(np.float32)
 
 
-def run_jolideco(datasets, config):
+def run_jolideco(datasets, config, debug):
     """Run jolideco"""
     from jolideco.core import MAPDeconvolver
     from jolideco.models import FluxComponents
@@ -53,15 +53,15 @@ def run_jolideco(datasets, config):
 
     deconvolver = MAPDeconvolver(**config["deconvolver"])
 
-    if DEBUG:
-        deconvolver.n_epochs = 5
+    if debug:
+        deconvolver.n_epochs = N_ITER_DEBUG
 
     datasets = prepare_datasets_jolideco(datasets=datasets)
     result = deconvolver.run(datasets=datasets, components=components)
     return result
 
 
-def run_pylira(datasets, config):
+def run_pylira(datasets, config, debug):
     """Run LIRA"""
     from pylira import LIRADeconvolver
 
@@ -71,24 +71,25 @@ def run_pylira(datasets, config):
 
     deconvolver = LIRADeconvolver(**config["deconvolver"])
 
-    if DEBUG:
-        deconvolver.n_iter_max = 5
+    if debug:
+        deconvolver.n_iter_max = N_ITER_DEBUG
 
     result = deconvolver.run(data=dataset)
     return result.reduce_to_mean_std()
 
 
-def run_deconvolution(datasets, config_run):
+def run_deconvolution(datasets, config_run, debug):
     """Run deconvolution"""
     if config_run["method"] == "jolideco":
-        return run_jolideco(datasets, config=config_run)
+        return run_jolideco(datasets, config=config_run, debug=debug)
     elif config_run["method"] == "lira":
-        return run_pylira(datasets, config=config_run)
+        return run_pylira(datasets, config=config_run, debug=debug)
     else:
         raise ValueError(f"Unknown method: {config_run['method']}")
 
 
 if __name__ == "__main__":
+    debug = snakemake.config["debug"]
     config = read_config(snakemake.input.config)
 
     datasets = read_datasets(
@@ -97,5 +98,7 @@ if __name__ == "__main__":
     )
 
     for idx, config_run in enumerate(config["runs"]):
-        result = run_deconvolution(datasets=datasets, config_run=config_run)
+        result = run_deconvolution(
+            datasets=datasets, config_run=config_run, debug=debug
+        )
         result.write(snakemake.output[idx])
